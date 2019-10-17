d448e41ac9359c3a9a0c06f0654f6f8b455bd2df
DarioSindicic <dario.sindicic@gmail.com>
2019-10-17 21:13:34 +0200

    client structure

---
+++ lab1/client.h
@@ -0,0 +1,42 @@
+#include <string.h>
+#define LEN_USERNAME 100
+#define LEN_PARAMETER 100
+#define LEN_IP_ADDRESS 20
+
+class Sensor{
+    private:
+        char username[LEN_USERNAME];
+        double latitude;
+        double longitude;
+        char ipAddress[LEN_IP_ADDRESS];
+        size_t port;
+    public:
+    Sensor(char *username, double latitude, double longitude, char *ipAddress, size_t port) {
+        strcpy(this->username, username);
+        this->latitude = latitude;
+        this->longitude = longitude;
+        this->port = port;
+        strcpy(this->ipAddress, ipAddress);
+    }
+    float getLatitude() {
+        return latitude;
+    }
+    float getLongitude() {
+        return longitude;
+    }
+};
+
+class Measurement{
+    private:
+        char username[LEN_USERNAME];
+        char parameter[LEN_PARAMETER];
+        float averageValue;
+    Measurement(char *username, char *parameter, float averageValue) {
+        strcpy(this->username, username);
+        strcpy(this->parameter, parameter);
+        this->averageValue = averageValue;
+    }
+    float getValue(int time, char* parameter);
+
+
+};
